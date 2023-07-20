26f83ef9ac21d69f6b647557f339eac3cf51ead7
priyankarastogi84 <priyanka.rastogi84@gmail.com>
2023-07-20 13:55:48 -0500

    baseclass

--- src/main/java/resources/Baseclass.java
+++ src/main/java/resources/Baseclass.java
@@ -1,17 +1,26 @@
 package resources;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 public class Baseclass {
 
-	public WebDriver driver;
+	public static WebDriver driver;
 	public Properties prop;
 
 	public void initializerDriver() throws IOException {
@@ -44,6 +53,32 @@ public class Baseclass {
 			System.out.println("please choose proper browser");
 		}
 
+	}
+
+	@BeforeTest
+	public void ExtentReport() {
+		extentManager.setup();
+	}
+
+	@AfterTest
+	public void endReport() {
+		extentManager.endReport();
+	}
+
+	// To take the screenshot and store in one folder-
+	public static String screenShot(WebDriver driver, String filename) {
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		String destination = System.getProperty("user.dir") + "\\ScreenShot\\" + filename + "_" + dateName + ".png";
+		File finalDestination = new File(destination);
+		try {
+			FileUtils.copyFile(source, finalDestination);
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		return destination;
 	}
 
 	@BeforeMethod
